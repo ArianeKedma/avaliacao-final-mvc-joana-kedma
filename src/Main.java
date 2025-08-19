2f93336b8977768cd05edfad955ce4a831742c10
ArianeKedma <ariane_k_silva@estudante.sesisenai.org.br>
2025-08-18 21:16:29 -0300

    Atualizações do dia 18/08/2025

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
         TarefaController controller = new TarefaController();
 
-        controller.adicionar("Estudar Padrões", "Estudar Factory", Prioridade.ALTA, LocalDate.now().plusDays(1));
-        controller.adicionar("Fazer almoço", "Cozinhar arroz", Prioridade.BAIXA, LocalDate.now().minusDays(1));
-        controller.concluir("Estudar Padrões");
+        controller.adicionar("Estudar pra Prova do Quost", "Estudar Front", Prioridade.ALTA, LocalDate.now().plusDays(1));
+        controller.adicionar("Fazer almoço", "Fazer macarrão", Prioridade.BAIXA, LocalDate.now().minusDays(1));
+        controller.concluir("Estudar Arquitetura de Sistemas");
 
         System.out.println("Todas:");
         controller.listar().forEach(System.out::println);
